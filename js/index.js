4220c8c638d8aa5d2e47acbbd12dea49e383fe0e
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-04-11 05:16:20 +0200

    update all

--- js/index.js
+++ js/index.js
@@ -11,20 +11,32 @@ const btnReset = document.querySelector(".btn-reset");
 const timer = document.querySelector(".timer");
 const coins = document.querySelector(".coins");
 const coin = document.querySelector(".slide-top");
+
+const recordList = document.querySelector(".error");
+const timerCount = document.querySelector(".timerCount");
+
 const KEY = "coins";
 
-let textList =
+const lengthCount = document.querySelector(".length-count");
+export let textList =
   "The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was that somebody would discover it. They didn’t think they could bear it if anyone found out about the Potters. Mrs. Potter was Mrs. Dursley’s sister, but they hadn’t met for several years; in fact, Mrs. Dursley pretended she didn’t have a sister, because her sister and her good-for-nothing husband were as unDursleyish as it was possible to be. The Dursleys shuddered to think what the neighbors would say if the Potters arrived in the street. The Dursleys knew that the Potters had a small son, too, but they had never even seen him. This boy was another good reason for keeping the Potters away; they didn’t want Dudley mixing with a child like that.";
-let devList =
+export let devList =
   "The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was that somebody would discover it. They didn’t think they could bear it if anyone found out about the Potters. Mrs. Potter was Mrs. Dursley’s sister, but they hadn’t met for several years; in fact, Mrs. Dursley pretended she didn’t have a sister, because her sister and her good-for-nothing husband were as unDursleyish as it was possible to be. The Dursleys shuddered to think what the neighbors would say if the Potters arrived in the street. The Dursleys knew that the Potters had a small son, too, but they had never even seen him. This boy was another good reason for keeping the Potters away; they didn’t want Dudley mixing with a child like that.";
 let error = 0;
 let speed = 0;
 let oldSpeed = 0;
+let allWords = 0;
+let wordsError = {
+  data: "",
+  state: true,
+};
+let beforeWordsError = "";
 
-checlLocalStorage();
+checkLocalStorage();
 closeIco.addEventListener("click", (e) => {
   backdrop.classList.add("is-hidden");
 });
+
 document.body.addEventListener("keydown", (e) => {
   console.log(e);
   if (btn.disabled === true) {
@@ -35,6 +47,8 @@ document.body.addEventListener("keydown", (e) => {
       e.key === "Shift" ||
       e.key === "Alt" ||
       e.key === "Control" ||
+      e.key === "AltGraph" ||
+      e.key === "Tab" ||
       e.key != textList[0]
     ) {
       return;
@@ -52,7 +66,7 @@ function reload() {
   location.reload();
 }
 
-function checlLocalStorage() {
+function checkLocalStorage() {
   if (localStorage.getItem(KEY)) {
     coins.textContent = localStorage.getItem(KEY);
     speed = JSON.parse(localStorage.getItem(KEY));
@@ -64,6 +78,7 @@ function checkInput() {
   if (inputUserText.value !== "") {
     devList = inputUserText.value;
     textList = inputUserText.value;
+    allWords = inputUserText.value;
     inputText.value = inputUserText.value;
     inputUserText.value = "";
   }
@@ -92,12 +107,16 @@ btn.addEventListener("click", (event) => {
       clearInterval(checkList);
       clearInterval(checTime);
       localStorage.setItem(KEY, coins.textContent);
+      backdrop.classList.remove("is-hidden");
+      lengthCount.textContent = allWords.length;
+      recordList.textContent = error;
+      timerCount.textContent = timer.textContent;
 
       if (sec >= textList.length / 3) {
         console.log(textList.length / 3);
       }
       // меньше чем 12 и больше чем 10
-      else if (sec <= textList.length / 3.5 && sec >= testList.length / 4) {
+      else if (sec <= textList.length / 3.5 && sec >= textList.length / 4) {
       }
       //
       else if (sec < textList.length / 4) {
@@ -113,9 +132,12 @@ function indexPlus(key) {
   inputText.value = devList;
 
   if (key === textList[0]) {
+    wordsError.data += textList[0];
     devList = devList.slice(1);
     inputText.value = devList;
-
+    if (key === " ") {
+      console.log(wordsError.data);
+    }
     return;
   }
 }
@@ -128,23 +150,22 @@ function pushError(key) {
     key === "Alt" ||
     key === "Control" ||
     key === "AltGraph" ||
+    key === "Tab" ||
     key === textList[0]
   ) {
     return;
   } else {
-    if (speed === 0) {
-      error += 1;
-      return;
-    } else {
-      error += 1;
-      speed -= 1;
-      console.log(error);
-      coins.textContent = speed;
-      return;
-    }
+    error += 1;
+    speed -= 1;
+    console.log(error);
+
+    coins.textContent = speed;
+
+    return;
   }
 }
-//Проверка совпадает написанная буква с ткстом или нет
+
+//Проверка совпадает написанная буква с тeкстом или нет
 function checText(key) {
   if (textList[0] === key) {
     textList = textList.slice(1);
@@ -191,4 +212,4 @@ function addCoin() {
     coin.classList.add("slide-top");
   }, 0);
 }
-console.log("length text: " + textList.length + " words");
+console.log("length text: " + textList.length + " sumbols");
