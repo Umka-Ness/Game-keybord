f7e4c98172d45b0938bd88e4677e7f138d2234f4
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-05-08 15:08:16 +0200

    Revert "update Progress-bar in the modal"
    This reverts commit 18f667d14bd179ce4808b47aba6fb27d4d3e2d3a.

--- js/index.js
+++ js/index.js
@@ -144,7 +144,6 @@ btn.addEventListener("click", (event) => {
       timerCount.textContent = timer.textContent;
       console.log(wordsError);
       counterDoneWords();
-      grade();
 
       addErrorsInModalErrors();
       if (sec >= textList.length / 3) {
@@ -296,17 +295,5 @@ function zeroCoins() {
 function counterDoneWords() {
   countDone = Math.round(((copyTextList - error) * 100) / copyTextList);
   countProcentDone.textContent = countDone + " %";
-  console.log(countDone);
-}
-
-function grade() {
-  const progress = document.querySelector(".Progress");
-  if (countDone >= 95) {
-    progress.textContent = "Awesome";
-  } else if (countDone <= 94 && countDone >= 80) {
-    progress.textContent = " Super";
-  } else if (countDone <= 79 && countDone >= 60) {
-    progress.textContent = "Not bad";
-  }
   console.log(countDone);
 }
