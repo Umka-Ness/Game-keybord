cc80a3b5fa5eea6a10d7041a124dd07fdf23f027
Umka_Ness <melnik.dmitri2012@gmail.com>
2023-05-10 00:47:37 +0200

    update all

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 import "../src/Game-Keybord/js/index";
 
 import App from "./App";
-
+console.log("Hello");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
